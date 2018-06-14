e1e060a3bf4f82b6c4a7f1a701d759a256b3c6b7
Wajeeh <wajeeh.zantout@gmail.com>
2018-06-14 16:38:04 +0300

    feat: add get all employees

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const { GraphQLServer } = require("graphql-yoga");
+const { Prisma } = require("prisma-binding");
+require("dotenv").config();
+
 const Query = require("./resolvers/Query");
 
 const resolvers = {
@@ -7,7 +10,16 @@ const resolvers = {
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: "src/generated/prisma.graphql",
+      endpoint: process.env.PRISMA_ENDPOINT,
+      secret: process.env.PRISMA_SECRET,
+      debug: true
+    })
+  })
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,6 +2,11 @@ function info() {
   return "API for Employee Directory";
 }
 
+function employees(root, args, context, info) {
+  return context.db.query.employees({}, info);
+}
+
 module.exports = {
-  info
+  info,
+  employees
 };
