d6b99e42cb1ce18a13e6b2618ba4b0ef0628b30e
Wajeeh <wajeeh.zantout@gmail.com>
2018-06-18 15:17:02 +0300

    feat: get employee by id

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -6,7 +6,18 @@ function employees(root, args, context, info) {
   return context.db.query.employees({}, info);
 }
 
+function employee(root, args, context, info) {
+  const { id } = args;
+  return context.db.query.employee(
+    {
+      where: { id }
+    },
+    info
+  );
+}
+
 module.exports = {
   info,
-  employees
+  employees,
+  employee
 };
